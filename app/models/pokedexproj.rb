c07969126e996ea9f99a884fe537816cd77eac96
+ <mcrea23@aol.com>
2021-01-04 12:45:53 -0600

    Created models

--- app/models/pokedexproj.rb
+++
@@ -1,2 +0,0 @@
-class POKEDEXPROJ < ActiveRecord::Base
-end
--- db/migrate/20210104175511_create_pokedexprojs.rb
+++
@@ -1,8 +0,0 @@
-class CreatePokedexprojs < ActiveRecord::Migration
-  def change
-    create_table :pokedexprojs do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
